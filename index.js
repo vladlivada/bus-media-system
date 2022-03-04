a8e8a8e12b71898e97ff7287aa7c59cf32c708fe
DESKTOP-85G49VG\Claudiu <Claudiu.Badea@externals.adidas.com>
2022-03-04 23:53:38 +0100

    add connection to odb mock

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 const path = require('path');
 var http = require("http");
 const { initSocket, sendMessage } = require('./routes/websocket');
@@ -19,6 +20,28 @@ const server = http.createServer(app);
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 initSocket(server);
 
+let lastAlertStop = null;
+
 setInterval(() => {  
-  sendMessage('Urmeaza statia Muncii');
-}, 60000)
+
+  axios
+    .get(globalConfig.rtpi.host + globalConfig.rtpi.endpoint)
+    .then(res => {
+      let rtpidata = res.data;
+      let visitedStops = rtpidata.times.filter(i => i.status == 3);
+      if (visitedStops && visitedStops.length > 0) {
+        let lastStop = visitedStops.sort((a,b) => b.index - a.index)[0]
+        if (lastStop.stop.id != lastAlertStop?.stop?.id) {
+          lastAlertStop = lastStop;
+          let nextStop = rtpidata.times.find(s => s.index === lastStop.index + 1);
+          if (nextStop) {
+            console.log("urmeaza statia " + nextStop.stop.name);
+            sendMessage("urmeaza statia " + nextStop.stop.name);
+          }
+        }
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}, globalConfig.rtpi?.pollIntervalMs || 5000)
