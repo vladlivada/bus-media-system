e2b041c943c4606c91972a940e733e44f2700622
DESKTOP-85G49VG\Claudiu <Claudiu.Badea@externals.adidas.com>
2022-03-10 22:13:31 +0100

    release 1.0.0

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 4202
 var fs = require('fs');
 
 var globalConfig = JSON.parse(fs.readFileSync('resources/config.json', 'utf8'));
+if (globalConfig == null || globalConfig.rtpi == null || globalConfig.rtpi.host == null) {
+  throw "Unable to read config data, please check config";
+}
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -21,10 +24,13 @@ server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 initSocket(server);
 
 let lastAlertStop = null;
+let requestConfig = {
+  headers: globalConfig.rtpi.headers
+}
 
 setInterval(() => {  
   axios
-    .get(globalConfig.rtpi.host + globalConfig.rtpi.endpoint)
+    .get(globalConfig.rtpi.host + globalConfig.rtpi.endpoint, requestConfig)
     .then(res => {
       let rtpidata = res.data;
       let visitedStops = rtpidata.times.filter(i => i.status == 3);
@@ -34,7 +40,7 @@ setInterval(() => {
           lastAlertStop = lastStop;
           let nextStop = rtpidata.times.find(s => s.index === lastStop.index + 1);
           if (nextStop) {
-            console.log("Sending new stop" + nextStop.stop.name);
+            console.log("Sending new stop " + nextStop.stop.code);
             sendMessage(JSON.stringify({
               type: "station",
               id: nextStop.stop.code
@@ -44,6 +50,6 @@ setInterval(() => {
       }
     })
     .catch(err => {
-      console.error(err);
+      console.error("Error pinging RTPI server: ", err.message)
     })
 }, globalConfig.rtpi?.pollIntervalMs || 5000)
